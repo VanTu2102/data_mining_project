048d53aa2f8d61f1e57f96d606168899fb3a1c58
DESKTOP-KJNF2QE\Admin <vantu2102@gmail.com>
2024-05-03 00:25:25 +0700

    .

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
-from sklearn.decomposition import FactorAnalysis
+from apyori import apriori, load_transactions
 
-def add_col(df, column): # Thêm cột lần đăng ký là lần bao nhiêu và nhãn dữ liệu có tái tục hay không tương ứng với mỗi dòng dữ liệu
+
+def add_col(
+    df, column
+):  # Thêm cột lần đăng ký là lần bao nhiêu và nhãn dữ liệu có tái tục hay không tương ứng với mỗi dòng dữ liệu
     for i in range(len(df) - 1):
         for j in range(len(df) - i - 1):
             if datetime.datetime.strptime(
-                df.iloc[j][column], "%m/%d/%Y %H:%M"
-            ) > datetime.datetime.strptime(df.iloc[j + 1][column], "%m/%d/%Y %H:%M"):
+                df.iloc[j][column], "%Y-%m-%d"
+            ) > datetime.datetime.strptime(df.iloc[j + 1][column], "%Y-%m-%d"):
                 df.iloc[j], df.iloc[j + 1] = df.iloc[j + 1], df.iloc[j]
     column_index = [(i + 1) for i in range(len(df))]
     df.insert(len(df.columns), "Lần đăng ký", column_index)
@@ -22,18 +25,74 @@ def add_col(df, column): # Thêm cột lần đăng ký là lần bao nhiêu và
     return df
 
 
+def apri(df, threshold=0.2):
+    new_data = []
+    last_id = None
+    for j in range(len(df)):
+        if df.iloc[j]["Mã học viên"] != last_id:
+            last_id = df.iloc[j]["Mã học viên"]
+            new_data.append(
+                {
+                    "Mã học viên": last_id,
+                    "Khóa học": [df.iloc[j]["Khóa học"]],
+                    "Người đăng ký": [df.iloc[j]["Người đăng ký"]],
+                    "Khuyến mãi (khoá tiếp theo))": [
+                        df.iloc[j]["Khuyến mãi (khoá tiếp theo))"]
+                    ],
+                    "Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)": [
+                        df.iloc[j][
+                            "Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)"
+                        ]
+                    ],
+                    "Mã giảng viên": [df.iloc[j]["Mã giảng viên"]],
+                    "Số kỹ năng khoá học": [df.iloc[j]["Số kỹ năng khoá học"]],
+                }
+            )
+        else:
+            new_data[-1]["Khóa học"].append(df.iloc[j]["Khóa học"])
+            new_data[-1]["Người đăng ký"].append(df.iloc[j]["Người đăng ký"])
+            new_data[-1]["Khuyến mãi (khoá tiếp theo))"].append(
+                df.iloc[j]["Khuyến mãi (khoá tiếp theo))"]
+            )
+            new_data[-1][
+                "Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)"
+            ].append(
+                df.iloc[j]["Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)"]
+            )
+            new_data[-1]["Mã giảng viên"].append(df.iloc[j]["Mã giảng viên"])
+            new_data[-1]["Số kỹ năng khoá học"].append(
+                df.iloc[j]["Số kỹ năng khoá học"]
+            )
+    new_data = {
+        "Khóa học": [d["Khóa học"] for d in new_data],
+        "Người đăng ký": [d["Người đăng ký"] for d in new_data],
+        "Khuyến mãi (khoá tiếp theo))": [
+            d["Khuyến mãi (khoá tiếp theo))"] for d in new_data
+        ],
+        "Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)": [
+            d["Số kỹ năng học viên muốn đăng ký(nghe, nói, đọc, viết)"]
+            for d in new_data
+        ],
+        "Mã giảng viên": [d["Mã giảng viên"] for d in new_data],
+        "Số kỹ năng khoá học": [d["Số kỹ năng khoá học"] for d in new_data],
+    }
+    for key, value in new_data.items():
+        print(pd.DataFrame(apriori(value)))
+    print(df)
+
+
 def etl():
     # Mở file data
     data_df = pd.read_csv("./data/data.csv", delimiter=",")
 
     # Transform data chuyển dữ liệu về số
     km = {"15%": 15, "8%": 8, "5%": 5, np.nan: np.nan}
-    data_df["Khuyến mãi (khoá tiếp theo))"] = data_df["Khuyến mãi (khoá tiếp theo))"].map(
-        km
-    )
+    data_df["Khuyến mãi (khoá tiếp theo))"] = data_df[
+        "Khuyến mãi (khoá tiếp theo))"
+    ].map(km)
     kq = {"Đạt": 1, "Không đạt": 0, np.nan: np.nan}
     data_df["Kết quả sau khi học"] = data_df["Kết quả sau khi học"].map(kq)
-    
+
     data_df["Học phí"] = data_df["Học phí"] / 1000000
 
     # Loại bỏ các cột thừa
@@ -51,55 +110,56 @@ def etl():
         inplace=True,
     )
 
-    
     # Nhóm dữ liệu theo mã học viên và giữ lại các cột cần thiết
     data_df = data_df.groupby("Mã học viên")
     last_data = pd.concat([add_col(group, "Ngày đăng ký") for name, group in data_df])
-    
+
     data_return = last_data.loc[last_data['Có tái tục không'] == 1]
     data_non_return = last_data.loc[last_data['Có tái tục không'] == 0]
     
-    print(last_data)
-    print(data_return.describe())
-    data_return.describe().to_excel("./results/data_return_describe.xlsx", index=True)
-    print(data_return.describe(include=['object']))
-    data_return.describe(include=['object']).to_excel("./results/data_return_describe_obj.xlsx", index=True)
-    
-    print(data_non_return.describe())
-    data_non_return.describe().to_excel("./results/data_non_return_describe.xlsx", index=True)
-    print(data_non_return.describe(include=['object']))
-    data_non_return.describe(include=['object']).to_excel("./results/data_non_return_describe_obj.xlsx", index=True)
-    
-    plt.figure(figsize=(16,6))
-    plt.hist([data_return["Khóa học"], data_non_return["Khóa học"]], bins=10,  density=True
-             , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
-    plt.legend(['Có tái tục', 'Không tái tục'])
-    plt.savefig("results/images/hist_data.png")
-    plt.clf()
-    
-    plt.figure(figsize=(6,6))
-    plt.hist([data_return["Lần đăng ký"], data_non_return["Lần đăng ký"]], bins=10
-             , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
-    plt.legend(['Có tái tục', 'Không tái tục'])
-    plt.savefig("results/images/hist_data_2.png")
-    plt.clf()
-    
-    plt.figure(figsize=(6,6))
-    plt.hist([data_return["Học phí"], data_non_return["Học phí"]], bins=10
-             , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
-    plt.legend(['Có tái tục', 'Không tái tục'])
-    plt.savefig("results/images/hist_data_3.png")
-    plt.clf()
-    
-    last_data.boxplot(column=[
-            "Lần đăng ký",
-            "Đánh giá giảng viên",
-            "Học phí",
-            "Kết quả sau khi học",
-            "Khuyến mãi (khoá tiếp theo))"], by='Có tái tục không', fontsize=10, grid=True, figsize=(10,6))
-    plt.savefig("results/images/boxplot.png")
-    plt.clf()
-    
+    apri(data_return)
+
+    # print(last_data)
+    # print(data_return.describe())
+    # data_return.describe().to_excel("./results/data_return_describe.xlsx", index=True)
+    # print(data_return.describe(include=['object']))
+    # data_return.describe(include=['object']).to_excel("./results/data_return_describe_obj.xlsx", index=True)
+
+    # print(data_non_return.describe())
+    # data_non_return.describe().to_excel("./results/data_non_return_describe.xlsx", index=True)
+    # print(data_non_return.describe(include=['object']))
+    # data_non_return.describe(include=['object']).to_excel("./results/data_non_return_describe_obj.xlsx", index=True)
+
+    # plt.figure(figsize=(16,6))
+    # plt.hist([data_return["Khóa học"], data_non_return["Khóa học"]], bins=10,  density=True
+    #          , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
+    # plt.legend(['Có tái tục', 'Không tái tục'])
+    # plt.savefig("results/images/hist_data.png")
+    # plt.clf()
+
+    # plt.figure(figsize=(6,6))
+    # plt.hist([data_return["Lần đăng ký"], data_non_return["Lần đăng ký"]], bins=10
+    #          , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
+    # plt.legend(['Có tái tục', 'Không tái tục'])
+    # plt.savefig("results/images/hist_data_2.png")
+    # plt.clf()
+
+    # plt.figure(figsize=(6,6))
+    # plt.hist([data_return["Học phí"], data_non_return["Học phí"]], bins=10
+    #          , histtype='bar', stacked=False, color=['cyan', 'green'], edgecolor='black')
+    # plt.legend(['Có tái tục', 'Không tái tục'])
+    # plt.savefig("results/images/hist_data_3.png")
+    # plt.clf()
+
+    # last_data.boxplot(column=[
+    #         "Lần đăng ký",
+    #         "Đánh giá giảng viên",
+    #         "Học phí",
+    #         "Kết quả sau khi học",
+    #         "Khuyến mãi (khoá tiếp theo))"], by='Có tái tục không', fontsize=10, grid=True, figsize=(10,6))
+    # plt.savefig("results/images/boxplot.png")
+    # plt.clf()
+
     # sns.histplot(last_data, x='Đánh giá giảng viên', hue="Có tái tục không", multiple="dodge", shrink=.5, binwidth=1)
     # plt.show()
     last_data = last_data[
@@ -113,7 +173,7 @@ def etl():
             "Khuyến mãi (khoá tiếp theo))",
         ]
     ].dropna()
-    
+
     # sns.boxplot(data=data_return, color='teal', palette='rocket')
     # # sns.boxplot(data=data_non_return, color='tomato', palette='rocket')
 
@@ -124,7 +184,7 @@ def etl():
     # plt.show()
     # # Tính hệ số tương quan Pearson
     # correlation = last_data.corr()
-    
+
     # # Vẽ biểu đồ ma trận tương quan
     # sns.heatmap(correlation, annot=True)
     # plt.show()
--- src/main.py
+++ src/main.py
@@ -10,26 +10,26 @@ class Main:
         data = data_preprocessing.etl()
         # models = model_training.train(data)
         
-        X = pd.DataFrame(
-            data,
-            columns=[
-                "Lần đăng ký",
-                "Đánh giá giảng viên",
-                "Học phí",
-                "Kết quả sau khi học",
-                "Khuyến mãi (khoá tiếp theo))",
-            ],
-        )
-        y = pd.DataFrame(data, columns=["Có tái tục không"])
-        model_evaluation.report(
-        {
-            "data": {
-                "X_test": X,
-                "y_test": y,
-                "full": data,
-            },
-        }
-        )
+        # X = pd.DataFrame(
+        #     data,
+        #     columns=[
+        #         "Lần đăng ký",
+        #         "Đánh giá giảng viên",
+        #         "Học phí",
+        #         "Kết quả sau khi học",
+        #         "Khuyến mãi (khoá tiếp theo))",
+        #     ],
+        # )
+        # y = pd.DataFrame(data, columns=["Có tái tục không"])
+        # model_evaluation.report(
+        # {
+        #     "data": {
+        #         "X_test": X,
+        #         "y_test": y,
+        #         "full": data,
+        #     },
+        # }
+        # )
 
 
 if __name__ == "__main__":
